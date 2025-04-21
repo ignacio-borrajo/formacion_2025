29dceb3020574fd6f5cd8a58625602f46c7b4c7f
martinmh43 <martinmuherr@gmail.com>
2025-04-21 16:08:48 +0200

    fetch smth

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):
+    """
+    Model representing an expense
+    """
+    description = model.CharField(max_lenght=255)
+    limit = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(auto_created=True)
+    category = models.CharField(max_length=100, 
+        choices=[
+            ('FOOD', 'Food'),
+            ('TRANS', 'Transport'),
+            ('ENTR', 'Entretainment'),
+            ('UTIL', 'Utilities'),
+            ('OTHR', 'Other'),
+        ],
+    )
+
+    def __str__(self):
+        return f"{self.description} - {self.limit} - {self.date} - {self.category}"
