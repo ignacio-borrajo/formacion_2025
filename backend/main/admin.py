2dc9eb4d11e0ccccaa87d9345a18a8402afc3aff
Alberto Fernández Azcoaga <124345225+AlbertoFAz@users.noreply.github.com>
2025-04-21 16:40:26 +0200

    feat: creando un modelo

---
+++ backend/main/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from main.models import Expense
+
+# Register your models here.
+
+admin.site.register(Expense)
---
+++ backend/main/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Expense(models.Model):
+    """
+    Model represneting an expanse
+    """
+    description = models.CharField(max_length=255)
+    limit = models.DecimalField(max_digits=10, decimal_places=2,blank=True, null=True)
+    date = models.DateTimeField(auto_created=True)
+    category = models.CharField(max_length=100, choices=[
+        ('FOOD', 'Food'),
+        ('TRAN', 'Transport'),
+        ('ENTR', 'Entertaiment'),
+        ('UTIL', 'Utilities'),
+        ('OTHR', 'Other'),
+    ])
+
+    def __str__(self):
+        return f"{self.description} - {self.limit} - {self.date} - {self.category}"
