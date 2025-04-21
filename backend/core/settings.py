a24d33dd0528483b57f82f371ba9951bf248aaf5
Alberto Fernández Azcoaga <124345225+AlbertoFAz@users.noreply.github.com>
2025-04-21 18:58:41 +0200

    feat: ultimos cambios dia 1

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,6 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'es-es'
+
+LANGUAGES = [
+    ('es', _('Spanish')),
+    ('en', _('English'))
+]
 
 TIME_ZONE = 'Europe/Madrid'
 
