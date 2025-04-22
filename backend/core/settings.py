7996e932fd7ecbe120090161f5a3a2226d8cd701
Ignacio Borrajo <ignacio.borrajo@edisa.com>
2025-04-22 21:12:41 +0200

    chore: añadir launch.json para Windows

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -136,12 +136,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    # "DEFAULT_PERMISSION_CLASSES": [
-    #     "rest_framework_simplejwt.authentication.JWTAuthentication",
-    #     # "users.api_auth.CustomAuth",
-    # ]
     "DEFAULT_AUTHENTICATION_CLASSES": ("core.api_auth.CustomAuthentication",),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
