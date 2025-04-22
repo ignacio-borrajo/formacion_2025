d166397ceaaad875f7949222c6bfd124d90271ce
Ignacio Borrajo <ignacio.borrajo@edisa.com>
2025-04-22 19:50:03 +0200

    chore: eliminar prints y comentarios

--- backend/core/api_auth.py
+++ backend/core/api_auth.py
@@ -4,22 +4,11 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class CustomAuthentication(JWTAuthentication):
     def authenticate(self, request):
-
-        print("ENTRA")
-
         try:
-            # Intenta autenticar normalmente
             authenticated = super().authenticate(request)
             if authenticated:
-
-                print("AUTENTICADO")
-
                 return authenticated
 
-            print("NO AUTENTICADO")
-
-            # Si falla, permite usuarios anónimos
             return (AnonymousUser(), None)
         except Exception:
-            # En caso de cualquier error, permite usuarios anónimos
             return (AnonymousUser(), None)
