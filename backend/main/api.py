d5405d6f48278fd9cc364dce5a9c6f4243bbe6de
AlexPozas <97431370+AlexPozas@users.noreply.github.com>
2025-04-23 17:10:45 +0200

    r

--- backend/main/api.py
+++ backend/main/api.py
@@ -1,1 +1,12 @@
+from rest_framework import viewsets
+from main.controllers import get_expenses
+from main.serializers import ExpenseSerializer
 
+
+class ExpenseViewSet(viewsets.ModelViewSet):
+
+    serializer_class = ExpenseSerializer
+
+    def get_queryset(self):
+        queryset = get_expenses(self.request.user)
+        return queryset
