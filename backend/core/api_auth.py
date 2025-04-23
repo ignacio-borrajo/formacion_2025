91978668cdc76888ff25086f69d5160fc1d97976
AlexPozas <97431370+AlexPozas@users.noreply.github.com>
2025-04-23 09:26:44 +0200

    c

---
+++ backend/core/api_auth.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AnonymousUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+class CustomAuthentication(JWTAuthentication):
+    def authenticate(self, request):
+        try:
+            authenticated = super().authenticate(request)
+            if authenticated:
+                return authenticated
+
+            return (AnonymousUser(), None)
+        except Exception:
+            return (AnonymousUser(), None)
