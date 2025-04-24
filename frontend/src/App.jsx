0aac6908a772009bbeed01bc3925a251fc2509c3
Alexx915 <168471081+Alexx915@users.noreply.github.com>
2025-04-24 11:41:56 +0200

    Upgrade the UX/UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,27 +3,17 @@ import "./App.css";
 import Expenses from "./features/listados/pages/Expenses";
 import Login from "./features/users/pages/Login";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Layout from "./common/components/Layout";
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Inicio</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/gastos">Gastos</Link>
-          </li>
-        </ul>
-      </nav>
       <Routes>
-        <Route path="/" exact element={<Expenses />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/gastos" element={<Expenses />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" exact element={<Expenses />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/gastos" element={<Expenses />} />
+        </Route>
       </Routes>
     </Router>
   );
